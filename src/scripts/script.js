d3320cb9366b7b74db26a32f29aecb95cc533828
Marco Rettore <marcorettoremattes@gmail.com>
2022-02-19 23:34:43 -0300

    Day 3 Development

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,23 +2,50 @@ import { fetchData } from '../utils/fetchData.mjs';
 import { cardMaker } from '../utils/cardMaker.mjs';
 const API = 'https://rickandmortyapi.com/api/character/';
 
-const cardswrapper = document.getElementById('cards-wrapper');
-const selectList = document.getElementById('select-list');
-
-var maxPages = selectList.value;
+const cardswrapper = document.getElementById('cards-wrapper'); // node to append the childs
+var page = 1;  // default Page
 
 
 
-const getData = async () =>
+
+
+//Functions
+
+const getData = async (page) =>
 {
   try
   {
-    const { results } = await fetchData(API);
-    results.map (character =>
-      {
-        const card = cardMaker(character);
-        cardswrapper.appendChild(card);
-      })
+    const data = await fetchData(`${API}?page=${page}`); // all data
+    const { info } = await fetchData(`${API}?page=${page}`); // verify next and prev
+
+    while (cardswrapper.firstChild) // here we clean all nodes
+    { 
+      cardswrapper.removeChild(cardswrapper.lastChild);
+    }
+
+    data.results.map (character =>
+    {
+      const card = cardMaker(character); // llamamos a nuestra func
+      cardswrapper.appendChild(card); // pegamos las cards
+    })
+    
+    if(info.next === null) // button next check
+    {
+      buttonNext.style.display = "none";
+    }
+    else
+    {
+      buttonNext.style.display = "inline-block";
+    }
+
+    if(info.prev === null) //button prev check
+    {
+      buttonPrev.style.display = "none";
+    }
+    else
+    {
+      buttonPrev.style.display = "inline-block";
+    }
   }
   catch(error)
   {
@@ -26,20 +53,42 @@ const getData = async () =>
   }
 }
 
-const pagesNumber = (maxPages) =>
+const nextPage = () =>
 {
-  for(let i = 0 ; i<= maxPages ; i++)
+  if(page > 0 && page < 42)
   {
-    getData(i);
+    page += 1;
+    getData(page);
+    console.log(page);
   }
-};
-
-pagesNumber(maxPages);
-
-const random = () =>
-{
-  console.log(maxPages);
 }
 
-var button = document.getElementById('button-showme');
-button.addEventListener('click', random);
+const prevPage = () =>
+{
+  if(page > 1)
+  {
+    page -= 1;
+    getData(page);
+    console.log(page);
+  }
+}
+
+const listPage = () =>
+{
+  const listValue = Number(list.options[list.selectedIndex].value);
+  getData(listValue);
+  console.log('List Value tiene bug');
+}
+// Buttons
+
+const buttonNext = document.getElementById('button-next');
+buttonNext.addEventListener('click', nextPage);
+
+
+const buttonPrev = document.getElementById('button-prev');
+buttonPrev.addEventListener('click', prevPage);
+
+const list = document.getElementById('select-list');
+list.addEventListener('change', listPage);
+
+getData(page); // print all data
