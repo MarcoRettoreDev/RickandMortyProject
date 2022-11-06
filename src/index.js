a33dee5327549ba608a50bb5c1de63949ced68fc
Marco Rettore <marcorettoremattes@gmail.com>
2022-11-06 17:01:04 -0300

    Agrego archivos para PWA

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import ContextProvider from './Helpers/UseContext';
-import ReactQuery from './Helpers/ReactQuery';
-import { App } from './Components/App/App';
+import ContextProvider from "./Helpers/UseContext";
+import ReactQuery from "./Helpers/ReactQuery";
+import { App } from "./Components/App/App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-import bootstrap from 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import bootstrap from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <BrowserRouter>
     <ContextProvider>
       <ReactQuery>
-        <App/>
+        <App />
       </ReactQuery>
     </ContextProvider>
   </BrowserRouter>
 );
+
+serviceWorkerRegistration.register();
