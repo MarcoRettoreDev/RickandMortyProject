96683724d3d6f91f4288a227d57cb410510a53f4
MR.Dev <marcorettoremattes@gmail.com>
2023-05-26 01:50:32 -0300

    💄 Modifed size of card in mobile view

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,27 +1,30 @@
 import axios from "axios";
 import { useQuery } from "react-query";
 
-const Card = (props) =>
-{
-  const { name, img, status ,specie, episode, created, location } = props;
+const Card = (props) => {
+  const { name, img, status, specie, episode, created, location } = props;
 
-  const { isLoading, isError, data } = useQuery([`character-location-${episode}`], () =>{
-    if(episode !== undefined){
-      return axios.get(`https://rickandmortyapi.com/api/location/${episode}`)
-        .then(res => res.data)
+  const { isLoading, isError, data } = useQuery(
+    [`character-location-${episode}`],
+    () => {
+      if (episode !== undefined) {
+        return axios
+          .get(`https://rickandmortyapi.com/api/location/${episode}`)
+          .then((res) => res.data);
+      }
     }
-  })
+  );
 
-  if(isLoading){
-    return <h4>Loading location...</h4>
+  if (isLoading) {
+    return <h4>Loading location...</h4>;
   }
 
-  if(isError){
+  if (isError) {
     return <h4>Cant find this location</h4>;
   }
 
   return (
-    <article className=".ms-auto main__card-wrapper">
+    <article className=" main__card-wrapper">
       <div className="card-img-wrapper">
         <img src={img} alt={name}></img>
       </div>
@@ -32,20 +35,22 @@ const Card = (props) =>
         <div className="card-body">
           <div className="card-body-subheader">
             <p className={`${status.toLowerCase()} status`}></p>
-            <p>{status} - {specie}</p>
+            <p>
+              {status} - {specie}
+            </p>
           </div>
           <div className="card-body-text">
-            <p>{episode !== undefined ? 'First seen in:' : 'Created:'}</p>
+            <p>{episode !== undefined ? "First seen in:" : "Created:"}</p>
             <p>{episode !== undefined ? data.name : created.slice(0, 10)}</p>
           </div>
           <div className="card-body-text">
             <p>Location: </p>
-            <p>{location }</p>  
+            <p>{location}</p>
           </div>
         </div>
       </div>
     </article>
-  )
-}
+  );
+};
 
 export { Card };
